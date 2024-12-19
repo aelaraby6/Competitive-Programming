cca38f9c24c8da7721fa280be60f7a2c959b76f1
aelaraby6 <body1262005@gmail.com>
2024-12-19 15:32:22 +0200

    new leetcode problem solution

---
+++ leetcode/Array-String/Remove Element.cpp
@@ -0,0 +1,14 @@
+// Time complexity: O(n)
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int x = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[x++] = nums[i];
+            }
+        }
+        return x;
+    }
+};
