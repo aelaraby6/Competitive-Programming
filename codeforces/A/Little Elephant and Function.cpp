f430812a7a57329493257f960fee72a64f0bd72c
aelaraby6 <body1262005@gmail.com>
2025-08-29 22:53:00 +0300

    add new problem solution

---
+++ codeforces/A/Little Elephant and Function.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define Glitch ios_base::sync_with_stdio(false); cin.tie(0);
+using namespace std;
+
+
+
+int main() {
+    Glitch;
+
+    int n;
+    cin >> n;
+
+    cout<<n<<" ";
+
+    for (int i = 1; i < n  ; i++) {
+        cout<<i<<" ";
+    }
+
+
+
+    return 0;
+}
